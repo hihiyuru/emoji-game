f35851c1d02204d672578344aad5ea0eaff931b0
Una <hihi555000@gmail.com>
2023-08-04 19:59:33 +0800

    Initial Commit

---
+++ emoji-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emoji-game
+//
+//  Created by 徐于茹 on 2023/8/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
